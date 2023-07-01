1c1bae3cca856fd0c8256d5f906941b43074549e
Khant Sithu <khantseithuu@gmail.com>
2023-07-01 16:55:57 +0000

    initial commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>post</title>
+    <link rel="stylesheet" href="/app.css"/>
+</head>
+<body>
+    <article>
+        <h2><a href="/post">My First Post</a></h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas ipsam nobis, illum modi nesciunt porro amet aliquam doloribus blanditiis, quasi hic, omnis harum culpa assumenda recusandae soluta sapiente ducimus incidunt.</p>
+    </article>
+
+    <a href="/" class="btn">Go Back</a>
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+    <link rel="stylesheet" href="/app.css"/>
+    <script src="/index.js"></script>
+</head>
+<body>
+    <h1>Blog</h1>
+
+    <article>
+        <h2><a href="/post">My First Post</a></h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas ipsam nobis, illum modi nesciunt porro amet aliquam doloribus blanditiis, quasi hic, omnis harum culpa assumenda recusandae soluta sapiente ducimus incidunt.</p>
+    </article>
+    <article>
+        <h2><a href="/post">My Second Post</a></h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas ipsam nobis, illum modi nesciunt porro amet aliquam doloribus blanditiis, quasi hic, omnis harum culpa assumenda recusandae soluta sapiente ducimus incidunt.</p>
+    </article>
+    <article>
+        <h2><a href="/post">My Third Post</a></h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas ipsam nobis, illum modi nesciunt porro amet aliquam doloribus blanditiis, quasi hic, omnis harum culpa assumenda recusandae soluta sapiente ducimus incidunt.</p>
+    </article>
+</body>
+</html>
