0b61d8b3d77fbb7be3f411d24a69a811e9537b00
Khant Sithu <khantseithuu@gmail.com>
2023-07-02 04:24:04 +0000

    wild card

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
 Route::get('/posts/{post}', function($slug) {
     $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
+
     if(! file_exists($path)) {
         return redirect('https://khantseithu-automatic-zebra-wqxp9j6465xhg5v5-8000.preview.app.github.dev/');
     }
@@ -29,4 +30,4 @@ Route::get('/posts/{post}', function($slug) {
     return view('post', [
         'post' => $post
     ]);
-});
+})->where('post', '[A-z_\-]+');
