b1ef65cafc55d3b0ff5a6bef33d419f816267151
Khant Sithu <khantseithuu@gmail.com>
2023-07-02 07:29:05 +0000

    blade layouts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,29 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog</title>
-    <link rel="stylesheet" href="/app.css"/>
-    <script src="/index.js"></script>
-</head>
-<body>
-    <h1>Blog</h1>
+@extends('layout')
+
+@section('banner')
+    <h1>My Blogs</h1>
+@endsection
+
+    @section('content')
 
     @foreach ($posts as $post)
-        {{-- @dd($loop) --}}
-        <article class="{{$loop->even ? "foo": ""}}">
-            <h1>
-                <a href="/posts/{{ $post->slug; }}">
-                    {{ $post->title;}}
-                </a>
-            </h1>
+    {{-- @dd($loop) --}}
+    <article class="{{$loop->even ? "foo": ""}}">
+        <h1>
+            <a href="/posts/{{ $post->slug; }}">
+                {{ $post->title;}}
+            </a>
+        </h1>
 
-            <div>
-                <?= $post->excerpt; ?>
-            </div>
-        </article>
+        <div>
+            <?= $post->excerpt; ?>
+        </div>
+    </article>
     @endforeach
-</body>
-</html>
+
+@endsection
