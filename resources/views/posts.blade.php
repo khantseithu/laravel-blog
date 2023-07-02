e700b1074a2d722a7455adb5481c36fc5879a1d0
Khant Sithu <khantseithuu@gmail.com>
2023-07-02 07:37:34 +0000

    blade component

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,24 +1,17 @@
-@extends('layout')
 
-@section('banner')
-    <h1>My Blogs</h1>
-@endsection
+<x-layout>
+@foreach ($posts as $post)
+{{-- @dd($loop) --}}
+<article class="{{$loop->even ? "foo": ""}}">
+    <h1>
+        <a href="/posts/{{ $post->slug; }}">
+            {{ $post->title;}}
+        </a>
+    </h1>
 
-    @section('content')
-
-    @foreach ($posts as $post)
-    {{-- @dd($loop) --}}
-    <article class="{{$loop->even ? "foo": ""}}">
-        <h1>
-            <a href="/posts/{{ $post->slug; }}">
-                {{ $post->title;}}
-            </a>
-        </h1>
-
-        <div>
-            <?= $post->excerpt; ?>
-        </div>
-    </article>
-    @endforeach
-
-@endsection
+    <div>
+        <?= $post->excerpt; ?>
+    </div>
+</article>
+@endforeach
+</x-layout>
