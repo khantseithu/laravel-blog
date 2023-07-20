4428f83f9b76335ae6d36c8b54fecfe95a665e78
Khant Sithu <khantseithuu@gmail.com>
2023-07-20 04:11:54 +0000

    feat: thumbnail

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,7 +1,10 @@
 <x-layout>
-    <section class="px-6 py-8">
+    <section class="max-w-md mx-auto px-6 py-8">
+        <h1 class="text-lg font-bold mb-4">
+            Publish New Post
+        </h1>
         <x-panel class="max-w-sm mx-auto">
-            <form method="POST" action="/admin/posts">
+            <form method="POST" action="/admin/posts" enctype="multipart/form-data">
                 @csrf
 
                 <div class="mb-6">
@@ -30,6 +33,23 @@
                     @enderror
                 </div>
 
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                           for="thumbnail"
+                    >
+                        Thumbnail
+                    </label>
+
+                    <input class="border border-gray-400 p-2 w-full"
+                           type="file"
+                           name="thumbnail"
+                           id="thumbnail"
+                    >
+
+                    @error('thumbnail')
+                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
+                </div>
 
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="excerpt">
